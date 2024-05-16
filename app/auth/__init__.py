73570e6de6f1ad51e1930b8ae460c15e8be1a586
memmalino <memmalino#gmail.com>
2024-05-16 22:05:41 +0100

    My first commit, updated login.py and register.py, created auth_base.html, auth_base_p.html as partial for auth_base.html, created login.html, register.html

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -6,3 +6,5 @@ from flask import Blueprint
 auth_views = Blueprint("auth_views", __name__)
 
 from . import login
+
+from . import register
--- app/forms.py
+++ app/forms.py
@@ -5,10 +5,21 @@
 from flask_wtf import FlaskForm
 from wtforms import (
     FileField,
-    SubmitField
+    SubmitField,
+    PasswordField,
+    StringField
 )
+from wtforms.validators import DataRequired, Email, Length
 
 
 class CreateProductForm(FlaskForm):
     file = FileField("Choose file")
+    submit = SubmitField('Submit')
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone_number = StringField('Phone Number', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Submit')
