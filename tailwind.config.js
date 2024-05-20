18848f9e9510fcaf122baf1094cc1bc6fa6206ae
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-05-20 18:52:20 +0100

    update: navbar, sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
         "pc-teal-normal": "#006060",
         "pc-teal-light": "#E6F2F2",
         "sc-gray-normal": "#F2F2F2",
+        "sc-gray": "#808080",
+        "sc-gray-light-hover": "#ECECEC",
         "sc-gray-dark": "#606060",
         "sc-gray-light-active": "#D8D8D8",
         "sc-gray-light": "#D8D8D8",
