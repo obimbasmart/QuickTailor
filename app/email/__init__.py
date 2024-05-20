7981c8d439aace84ffd92a2d552cb6c665516ef5
ObiamakaMaria <obiamaka0101@gmail.com>
2024-05-20 06:22:16 +0100

    testing sendgrid mail

--- app/email/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""initialize view module"""
-
-from flask import Blueprint
-
-email_views = Blueprint("email_views", __name__)
-
-from . import send_email
-from . import send_passowrd_mail
--- quicktailor.py
+++ quicktailor.py
@@ -1,9 +1,8 @@
 from app import app
 from app.views import app_views
 from app.auth import auth_views
-from app.email import email_views
+from app.sendgrid import email_service
 
 app.register_blueprint(app_views)
 app.register_blueprint(auth_views)
-app.register_blueprint(email_views)
-
+app.register_blueprint(email_service)
