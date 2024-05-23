1296f3fb331e333a04a7d2767d08de4c6827efa1
memmalino <memmalino#gmail.com>
2024-05-23 11:21:57 +0100

    Changes to app.models.base_user.py

--- app/models/base_user.py
+++ app/models/base_user.py
@@ -4,6 +4,10 @@
 """
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
+from datetime import datetime, timedelta
+from app import db
+
 
 
 
@@ -14,7 +18,18 @@ class BaseUser(UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def generate_reset_token(self, token):
+        self.reset_token = token
+        self.reset_token_expires = datetime.utcnow() + timedelta(hours=1)  # Token valid for 1 hour
+        db.session.commit()
 
+    def clear_reset_token(self):
+        self.reset_token = None
+        self.reset_token_expires = None
+        db.session.commit()
+~
+~
     @property
     def is_tailor(self):
         return False
