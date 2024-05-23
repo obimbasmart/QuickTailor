3d88d622d80ddda09d878dcea4a3ee03b46a0b95
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-05-23 21:24:49 +0100

    update: send email when user request for password reset

--- app/models/base_user.py
+++ app/models/base_user.py
@@ -7,8 +7,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
 from datetime import datetime, timedelta
 from app import db
+from app import app
+from itsdangerous import URLSafeTimedSerializer
 
-
+serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
 
 class BaseUser(UserMixin):
@@ -18,17 +20,35 @@ class BaseUser(UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
-    def generate_reset_token(self, token):
+
+    def generate_reset_token(self):
+        token = serializer.dumps(self.id, salt='password-reset-salt')
         self.reset_token = token
-        self.reset_token_expires = datetime.utcnow() + timedelta(hours=1)  # Token valid for 1 hour
+        self.reset_token_expires = datetime.utcnow(
+        ) + timedelta(hours=1)  # Token valid for 1 hour
         db.session.commit()
+        return token
 
     def clear_reset_token(self):
         self.reset_token = None
         self.reset_token_expires = None
         db.session.commit()
-   
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = serializer.loads(token, salt="password-reset-salt", max_age=3600)
+        except:
+            return None
+
+        from app.models.tailor import Tailor
+        from app.models.user import User
+
+        normal_user = db.session.get(User, id)
+        tailor = db.session.get(Tailor, id)
+
+        return normal_user or tailor
+    
     @property
     def is_tailor(self):
         return False
