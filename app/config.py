e0344e317f08e1cce36e153d689562c1b4424039
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-06-06 02:47:49 +0100

    update: MYSQL DB  configuration

--- app/config.py
+++ app/config.py
@@ -4,19 +4,15 @@
 """
 
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # TEMPLATES_AUTO_RELOAD = True
-    # SEND_FILE_MAX_AGE_DEFAULT = 0
 
-# Flask-Mail Configurations
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('SENDGRID_API_KEY')
     ADMINS = os.getenv('ADMINS')
