a93f6d630fcd7694d7c487a0d7a6d4971c595ca3
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-06-12 07:40:39 +0100

    refactor: switch to MySQL DB for dev and production

--- app/config.py
+++ app/config.py
@@ -12,14 +12,19 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('SENDGRID_API_KEY')
     ADMINS = os.getenv('ADMINS')
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://{}:{}@{}/{}'.format(os.environ.get("MYSQL_USER"),
+                                                                   os.environ.get(
+                                                                       "MYSQL_PWD"),
+                                                                   os.environ.get(
+                                                                       "MYSQL_HOST"),
+                                                                   os.environ.get(
+                                                                       "MYSQL_TEST_DB"))
+    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
