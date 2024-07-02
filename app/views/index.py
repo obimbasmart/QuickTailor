1d3206911a2a2e0313a604a7ceb8a0b276db97a1
memmalino <memmalino#gmail.com>
2024-07-02 11:53:49 +0100

    lattest chaange

--- app/views/index.py
+++ app/views/index.py
@@ -13,28 +13,6 @@ from app.models.tailor import Tailor
 from app.models.review import Review
 
 
-notification= [
-    {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "10, 000"},
-      {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "30,000"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "200, 000"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "1, 000, 000"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-    {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"},
-  {"time": "3hr ago", "content": "The virtues of life if I have the choice of life", "icon": "📥", "time_elapsed": "2 minutes ago"}
-
-]
 
 
 @app_views.route("/")
@@ -74,18 +52,6 @@ def register():
 def about_us():
     return render_template('pages/about_us.html')
 
-@app_views.route('/get_notifications')
-def get_notifications():
-    page = request.args.get('page', 1, type=int)
-    per_page = 10
-    start = (page - 1) * per_page
-    end = start + per_page
-    paginated_notifications = notification[start:end]
-    return jsonify(paginated_notifications)
-
-@app_views.route('/notification')
-def notificationn():
-    return render_template("pages/notification.html")
 
 
 
--- app/views/payment.py
+++ app/views/payment.py
@@ -2,13 +2,12 @@ from app.views import app_views
 from flask import render_template, request
 from app.constants import USER_SIDEBAR_LINKS
 # from cloud_storage.s3_cloud_storage import S3StorageService
-from app.views.index import notification
 from app.forms.cart_forms import CustomizationForm
 
 
 @app_views.route('/payment', methods=["GET", "POST"])
 def payment():
     form =  CustomizationForm()
-    return render_template('pages/payment.html', data=notification, form=form)
+    return render_template('pages/payment.html', form=form)
 
 
